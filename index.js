1f85ae3669692e7647a4603f6ec0569f71b5e2f6
Cleme Cajar <ccajarlaguna@gmail.com>
2025-05-17 16:35:10 -0500

    crecion de repositorio y exportacion de express

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // TODO: IMPORT MODULES
+const express = require('express'); // ? importamos express
+const morgan = require('morgan'); // ? importamos morgan
+const cors = require('cors'); // ? importamos cors
+const mongoose = require('mongoose'); // ? importamos mongoose
 
-import express from 'express';  
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import cors from 'cors';
-import bodyParser from "body-parser";
+const bodyParser = require('body-parser'); // ? importamos body-parser, un middleware para analizar el cuerpo de la peticion
 
 
-const app = express();
-app.use(bodyParser.json());
+const app = express(); // ?se usa la constante app para crear una instancia de express
+
+app.use(bodyParser.json());// ? analiza el cuerpo de la peticion y lo convierte a json
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
